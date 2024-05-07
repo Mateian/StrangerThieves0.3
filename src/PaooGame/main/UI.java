9e59341589c45ecaf37f0565a483933957c8c11a
Matei <bejmatei90@gmail.com>
2024-05-08 02:05:57 +0300

    Added Game Over Screen

--- src/PaooGame/main/UI.java
+++ src/PaooGame/main/UI.java
@@ -327,7 +327,6 @@ public class UI {
     }
 
     public void drawFPS(double FPS) {
-        // Draw FPS
         graph2.setColor(Color.white);
         graph2.setFont(console_40B);
         graph2.setFont(new Font("Consolas", Font.PLAIN, 20));
