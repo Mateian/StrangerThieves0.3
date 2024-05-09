541e5638b9a317a0a824d4470c122635cc3c0216
Matei <bejmatei90@gmail.com>
2024-05-09 13:03:39 +0300

    Modified Crop Function and Added Tiles

--- src/PaooGame/tiles/TileManager.java
+++ src/PaooGame/tiles/TileManager.java
@@ -18,7 +18,7 @@ public class TileManager {
     // Tile Array - all tiles in one array
     public Tile[] tile;
     // Tile SpriteSheet
-    public BufferedImage spriteSheet;
+    public BufferedImage spriteSheetMap1, spriteSheetMap2;
     // Map - a matrix with all tiles ID placed all over the width X height map
     public int[][] mapTile;
 
@@ -26,7 +26,8 @@ public class TileManager {
         this.gp = gp;
         tile = new Tile[20];
         try {
-            spriteSheet = ImageIO.read(getClass().getResourceAsStream("/tiles/level01/map01Tiles.png"));
+            spriteSheetMap1 = ImageIO.read(getClass().getResourceAsStream("/tiles/level01/map01Tiles.png"));
+            spriteSheetMap2 = ImageIO.read(getClass().getResourceAsStream("/tiles/level02/map02Tiles.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -38,27 +39,30 @@ public class TileManager {
     }
 
     public void getImage() {
-        setup(13, 0, 0, "flower_bush", false);
-        setup(0, 1, 0, "grass", false);
-        setup(11, 2, 0, "grass_fence", true);
-        setup(1, 3, 0, "gravel", false);
-        setup(12, 4, 0, "hidden_chest", false);
-        setup(3, 5, 0, "house_door_reversed", true);
-        setup(4, 6, 0, "house_wall", true);
-        setup(10, 7, 0, "plank_fence", true);
-        setup(5, 8, 0, "planks", false);
-        setup(6, 9, 0, "roof_left", true);
-        setup(7, 10, 0, "roof_middle", true);
-        setup(8, 11, 0, "roof_right", true);
-        setup(9, 12, 0, "stairs", false);
-        setup(2, 13, 0, "tree", true);
+        setup(13, 0, 0, spriteSheetMap1, false);    // flower_bush - 0
+        setup(0, 1, 0, spriteSheetMap1, false);     // grass - 1
+        setup(11, 2, 0, spriteSheetMap1, true);     // grass_fence - 2
+        setup(1, 3, 0, spriteSheetMap1, false);     // gravel - 3
+        setup(12, 4, 0, spriteSheetMap1, false);    // hidden chest - 4
+        setup(3, 5, 0, spriteSheetMap1, true);      // house_door_reversed - 5
+        setup(4, 6, 0, spriteSheetMap1, true);      // house_wall - 6
+        setup(10, 7, 0, spriteSheetMap1, true);     // plank_fence - 7
+        setup(5, 8, 0, spriteSheetMap1, false);     // planks - 8
+        setup(6, 9, 0, spriteSheetMap1, true);      // roof_left - 9
+        setup(7, 10, 0, spriteSheetMap1, true);     // roof_middle - 10
+        setup(8, 11, 0, spriteSheetMap1, true);     // roof_right - 11
+        setup(9, 12, 0, spriteSheetMap1, false);    // stairs - 12
+        setup(2, 13, 0, spriteSheetMap1, true);     // tree - 13
+        setup(14, 0, 0, spriteSheetMap2, false);    // ship floor - 14
+        setup(15, 1, 0, spriteSheetMap2, true);    // ship wall - 14
+        setup(16, 2, 0, spriteSheetMap2, true);    // space - 14
     }
 
-    public void setup(int index, int indexX, int indexY, String imagePath, boolean collision) {
+    public void setup(int index, int indexX, int indexY, BufferedImage image, boolean collision) {
         Tools tool = new Tools();
         try {
             tile[index] = new Tile();
-            tile[index].image = tool.cropImage(spriteSheet, gp.originalTileSize * indexX, gp.originalTileSize * indexY, gp.originalTileSize, gp.originalTileSize);
+            tile[index].image = tool.cropImage(image, gp.originalTileSize * indexX, gp.originalTileSize * indexY, gp.originalTileSize, gp.originalTileSize);
             tile[index].image = tool.scaleImage(tile[index].image, gp.tileSize, gp.tileSize);
             tile[index].collision = collision;
         } catch(Exception e) {
